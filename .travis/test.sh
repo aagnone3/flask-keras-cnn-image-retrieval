9c24a27703ca33d174a69bc44cd853843fdf9134
Anthony Agnone <aagnone@pindrop.com>
2017-10-23 21:08:05 -0400

    Re-org test.ush

--- .travis/test.sh
+++ .travis/test.sh
@@ -3,6 +3,12 @@
 # vars
 PYTEST_OPTIONS=-xs
 
+# add the package's root directory to the PYTHONPATH
+PYTHONPATH=${PYTHONPATH}:${TRAVIS_BUILD_DIR}
+
 # invocation
+echo ${PYTHONPATH}
+echo ${PATH}
+echo ${TRAVIS_BUILD_DIR}
 py.test ${PYTEST_OPTIONS} test
 
